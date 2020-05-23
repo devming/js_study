d6f362b096bec671d3f97314cc48d657cf3a4ce9
devming <cmk330@gmail.com>
2020-05-23 22:39:44 +0900

    other event

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const title = document.querySelector("#title");
+const content = document.querySelector("#ttttt");
 
 const BASE_COLOR = "rgb(52, 73, 94)";
 const OTHER_COLOR = "#7f8c8d";
+
+const seasons = {
+    SUMMER: 'summer',
+    WINTER: 'winter',
+    SPRING: 'spring',
+    AUTUMN: 'autumn'
+};
 
 function handleClick() {
     const currentColor = title.style.color;
@@ -12,9 +20,32 @@ function handleClick() {
     }
 }
 
+function detectWeather() {
+    const season = seasons.SPRING;
+    if (!season) {
+        throw new Error('Season is not defined');
+    }
+
+    switch (season) {
+        case season.SPRING:
+            console.log(`This is warm ${season}.`);
+        case seasons.SUMMER:
+            console.log(`This is hot ${season}.`);
+        case season.WINTER:
+            console.log(`This is cold ${season}.`);
+        case season.AUTUMN:
+            console.log(`This is cool ${season}.`);
+        default:
+            console.log(`This is ${season}.`);
+    }
+}
+
+
 function init() {
     title.style.color = BASE_COLOR;
     title.addEventListener("mouseenter", handleClick);
+    content.addEventListener("click", detectWeather);
+
 } 
 
 init();
