85ada535173e42c77653715ae401e10551b9d662
devming <cmk330@naver.com>
2020-05-26 19:12:43 +0900

    3.4강까지 따라함

---
+++ 3강연습/greeting.js
@@ -0,0 +1,40 @@
+const form = document.querySelector('.js-form');
+const input = form.querySelector('input');
+const greeting = document.querySelector('.js-greetings');
+
+const USER_LS = "currentUser",
+    SHOWING_ON = "showing";
+
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_ON);
+    form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreeting(text) {
+    form.classList.remove(SHOWING_ON);
+    greeting.classList.add(SHOWING_ON);
+    greeting.innerText = `Hello ${text}`;
+}
+
+function init() {
+    const currentUser = localStorage.getItem(USER_LS);
+
+    if (currentUser === null) {
+        askForName();
+    } else {
+        paintGreeting(currentUser);
+    }
+}
+
+init();
