70be2894bc2c101596067e69d131ee0cc0cc542b
devming <cmk330@gmail.com>
2020-05-31 00:16:49 +0900

    3.6 까지 학습

---
+++ 3강연습/todo.js
@@ -0,0 +1,60 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+    toDoInput = toDoForm.querySelector("input"),
+    toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text) {
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    const span = document.createElement("span");
+    const newId = toDos.length + 1;
+
+    delBtn.innerText = "❌"
+    span.innerText = text;
+
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    li.id = newId;
+    toDoList.appendChild(li);
+
+    const toDoObj = {
+        text: text,
+        id: toDos.length + 1
+    };
+
+    toDos.push(toDoObj);
+
+    saveToDos();
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+}
+
+function loadToDos() {
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if (loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo) {
+            paintToDo(toDo.text);
+        });
+    }
+}
+
+function init() {
+
+    loadToDos();
+
+    toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
+
